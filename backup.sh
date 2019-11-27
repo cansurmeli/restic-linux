8f5f5d6a2ef3cfa0b1504d30ae40a2bd29efbbc0
Can Sürmeli <cansurmeli@outlook.com>
2019-11-27 10:33:19 +0100

    File naming scheme change.

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ source .00-restic_credentials
 
 sh ./.01-restic_backup.sh
 sh ./.02-restic_forget.sh
-sh ./.03-restic_check.sh
-sh ./.04-unset.sh
+sh ./.10-restic_forget.sh
+sh ./.11-restic_check.sh
 
 exit 0
