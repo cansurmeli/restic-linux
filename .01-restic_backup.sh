da0e393830fc6c24ca7f7fc557780ba85bb26f2a
Can Sürmeli <cansurmeli@outlook.com>
2022-02-21 13:56:35 +0000

    Added excluded directory explanations.

--- .01-restic_backup.sh
+++ .01-restic_backup.sh
@@ -3,14 +3,14 @@
 restic backup / \
 	--quiet \
 	--exclude-caches \
-	--exclude='/dev' \
-	--exclude='/lost+found' \
-	--exclude='/media' \
-	--exclude='/mnt' \
-	--exclude='/proc' \
-	--exclude='/run' \
-	--exclude='/sys' \
-	--exclude='/tmp' \
-	--exclude='/var/spool' \
-	--exclude='/var/run' \
-	--exclude='/var/tmp'
+	--exclude='/dev' \				# hardware conneccted
+	--exclude='/lost+found' \		# files due to unexpected shut-down
+	--exclude='/media' \			# temporary mount directory
+	--exclude='/mnt' \				# temporary mount directory
+	--exclude='/proc' \				# virtual file-system 
+	--exclude='/run' \				# early-runtime-dir
+	--exclude='/sys' \				# virtual filesystem
+	--exclude='/tmp' \				# temporary storage
+	--exclude='/var/spool' \		# temporary system files
+	--exclude='/var/run' \			# temporary system files
+	--exclude='/var/tmp'			# temporary system files
